2d4ccb1f85acd6a594aa3771ac1cc359f3c66144
Marc Nygaard <marcnygaard100@gmail.com>
2024-04-02 14:21:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
-const server=http.createServer((req, res) => {
-	res.write('Hej')
-	res.end()
+const server = http.createServer((req, res) => {
+	res.write('Hej');
+	res.end();
 })
 
 server.listen(3001, () => {
