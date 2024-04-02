d38c07be454f6f6dbdd0d4f93bbc118faac85abc
Kresten Laust <krestenlaust@gmail.com>
2024-04-02 11:30:09 +0200

    Add basic index

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server=http.createServer((req, res) => {
+	res.write('Hej')
+	res.end()
+})
+
+server.listen(3001, () => {
+	console.log('Server running on port 3001');
+});
+
+
