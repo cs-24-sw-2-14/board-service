ad951852fae1f29ea17fe5b3db88cc725fff92b2
Marc Nygaard <marcnygaard100@gmail.com>
2024-04-05 15:09:29 +0200

    small changes

---
+++ src/user.ts
@@ -0,0 +1,20 @@
+enum UserColor {
+    Red,
+    Blue,
+    Green,
+    Pink,
+    Orange,
+    Yellow,
+    Black,
+    White,
+    Purple,
+    Brown,
+    Cyan,
+    Lime
+}
+
+class User {
+    constructor(userName: string, preferedColor?: UserColor) {
+        
+    }
+}
