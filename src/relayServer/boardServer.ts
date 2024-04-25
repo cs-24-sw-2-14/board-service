e9393375e796add9a17f32b07d98e8e42ec1a920
Kresten Laust <krestenlaust@gmail.com>
2024-04-25 10:10:31 +0200

    Handle server messages

--- src/relayServer/boardServer.ts
+++ src/relayServer/boardServer.ts
@@ -1,12 +1,28 @@
-import { WebSocketServer, WebSocket } from "ws";
+import { WebSocketServer, WebSocket, MessageEvent } from "ws";
 
 namespace Backend.RelayServer {
 
   class BoardServer {
     wss: WebSocketServer;
+    boardClients: WebSocket[] = [];
 
     constructor(port: number){
       this.wss = new WebSocket.Server({ 'port': port });
+
+      this.wss.on('connection', (ws: WebSocket) => {
+        // Code to handle new WebSocket connections
+        this.boardClients.push(ws);
+
+        ws.on('close', (close: CloseEvent) => {
+          console.log("Closed");
+        });
+      });
+
+      this.wss.on('message', (message: MessageEvent) => {
+        // Code to handle incoming messages
+
+      });
+
     }
   }
 }
