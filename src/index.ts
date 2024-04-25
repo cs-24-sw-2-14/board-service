03237f8571552bde17a36d3b3a764bc472909d49
Kresten Laust <krestenlaust@gmail.com>
2024-04-25 14:05:52 +0200

    Initial commit in fix of entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-import { Server } from "./server";
+//import { Server } from "./server";
+const config = require('./restapi/config');
+const logger = require('./restapi/logger');
+const ExpressServer = require('./restapi/expressServer');
+//const server = new Server(8008);
+//server.StartServerAsync();
 
-const server = new Server(8008);
-server.StartServerAsync();
+
+const launchRestAPIServer = async () => {
+  try {
+    let expressServer = new ExpressServer(config.URL_PORT, config.OPENAPI_YAML);
+    expressServer.launch();
+    logger.info('Express server running');
+  } catch (error) {
+    logger.error('Express Server failure', error);
+    await close();
+  }
+};
+
+launchRestAPIServer().catch(e => logger.error(e));
