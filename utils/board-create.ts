d2ac4e4036e3ea7a9f66a818a14feacd687ca28c
Tbdlarsen <tbdlarsen@gmail.com>
2024-04-25 15:57:02 +0200

    created shell for CheckNewBoard function

--- utils/board-create.ts
+++ utils/board-create.ts
@@ -3,16 +3,31 @@
 
 export function GenerateBoard (){
   let newBoard = '';
-  for (let i = 0; i < 6; i++){
 
-    // creating new random board id in hexadecimal
-    let temp = Math.ceil(Math.random()*16).toString(16).toUpperCase();
+  while (true){
+
+    for (let i = 0; i < 6; i++){
+      // creating new random board id in hexadecimal
+      let temp = Math.ceil(Math.random()*15).toString(16).toUpperCase();
+      newBoard+= temp;
+
+    }
+
+    if (CheckNewBoard() === false){
+      //returns board id if it doesnt exist yet
+      return newBoard;
+
+    }
+
+}
 
 
-    newBoard+= temp;
+
   }
 
 
-  return newBoard;
+// TODO:  need to add function that checks if the newly generated board exists
+function CheckNewBoard(){
 
+  return false;
 }
