9ad2e9b3acd8dff0f427be39377af4a15f22f3eb
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-04-30 09:49:59 +0200

    Remove old files

--- src/boardUID.ts
+++
@@ -1,5 +0,0 @@
-/**
- * The ID which is uniquely associated with a board.
- * The customers connect to the board instance using it.
- */
-export type BoardUID = string;
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { Server } from "./server";
-
-const server = new Server(8008);
-server.StartServerAsync();
