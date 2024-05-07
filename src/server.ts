52e4ce2ba393706d185c1bc878d49ab2c45d5aa5
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-07 11:12:57 +0200

    Add endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,55 @@
 import express, { Express, Request, Response } from "express";
+import { createServer } from "http"
+var cors = require('cors');
 import { Server } from "socket.io";
 import { Boards } from "./boards"
 
 const app: Express = express();
+app.use(express.json());
+app.use(cors());
 const httpPort = 5123;
 
-const server = require('http').createServer(app);
-const socketio = new Server(server);
 const socketIoPort = 6123;
+const httpServer = createServer(app);
+const socketio = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
 
 let boards: Boards = new Boards(socketio)
 
 app.post("/v1/board/create", (_, res: Response) => {
-  const boardId = JSON.stringify(boards.createBoard())
-  res.send(boardId);
+  res.send({ boardId: boards.createBoard() });
+  return
 });
 
-app.get("/v1/board/join", (req: Request, res: Response) => {
-  const body = JSON.parse(req.body)
-  const board = boards.findBoard(body.boardID)
+app.post("/v1/board/join", (req: Request, res: Response) => {
+  const body = req.body
+  const board = boards.findBoard(body.boardId)
   if (!board) {
-    res.status(404)
-    res.send()
+    res.send("Board does not exist").status(404);
     return
   }
   const userExists = board.findUser(body.username)
   if (!userExists) {
     board.createUser(body.username)
+    res.send("Board is valid and username was added to board").status(200);
+    return
   }
-  res.status(200)
-  res.send()
+  res.send("Board and username is valid").status(200);
+  return
+});
+
+app.post("/v1/board/validate", (req: Request, res: Response) => {
+  const body = req.body
+  const board = boards.findBoard(body.boardId)
+  if (!board) {
+    res.send("Board does not exist").status(404);
+    return
+  }
+  res.send("Board is valid").status(200);
+  return
 });
 
 socketio.listen(socketIoPort);
