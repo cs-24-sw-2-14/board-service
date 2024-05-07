59112ad64a8b9b0d89ea6330dcde635766b87272
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-07 11:12:41 +0200

    Add eraseCommand type

--- src/commandController.ts
+++ src/commandController.ts
@@ -1,54 +1,61 @@
 import { Namespace } from "socket.io";
-import { DrawCommand } from "./commands/draw"
-import { MoveCommand } from "./commands/move"
+import { DrawCommand } from "./commands/draw";
+import { MoveCommand } from "./commands/move";
+import { EraseCommand } from "./commands/erase";
 
 export interface CommandInterface {
-  commandId: number
-  owner: string
-  execute: (socket: Namespace) => void
-  undo: (socket: Namespace) => void
-  redo: (socket: Namespace) => void
+  commandId: number;
+  owner: string;
+  execute: (socket: Namespace) => void;
+  undo: (socket: Namespace) => void;
+  redo: (socket: Namespace) => void;
 }
 
-export type Command = (DrawCommand | MoveCommand)
+// TODO: Implement type for a command, so we dont have to write DrawCommand | MoveCommand | EraseCommand
 
 export class CommandController {
-  undoStack: (DrawCommand | MoveCommand)[]
-  redoStack: (DrawCommand | MoveCommand)[]
+  undoStack: (DrawCommand | MoveCommand | EraseCommand)[];
+  redoStack: (DrawCommand | MoveCommand | EraseCommand)[];
   namespace: Namespace;
   constructor(namespace: Namespace) {
-    this.undoStack = []
-    this.redoStack = []
-    this.namespace = namespace
+    this.undoStack = [];
+    this.redoStack = [];
+    this.namespace = namespace;
   }
-  execute(command: (DrawCommand | MoveCommand)) {
-    command.execute(this.namespace)
-    this.redoStack = []
-    this.undoStack.push(command)
+  execute(command: DrawCommand | MoveCommand | EraseCommand) {
+    command.execute(this.namespace);
+    this.redoStack = [];
+    this.undoStack.push(command);
   }
   undo(username: string) {
-    if (this.undoStack.length === 0) return
-    const commandIndex = this.undoStack.findLastIndex((command: (DrawCommand | MoveCommand)) => command.owner === username)
-    if (!commandIndex) return
-    const command = this.undoStack.splice(commandIndex, 1)[0]
-    command.undo(this.namespace)
-    this.redoStack.push(command)
+    if (this.undoStack.length === 0) return;
+    const commandIndex = this.undoStack.findLastIndex(
+      (command: DrawCommand | MoveCommand | EraseCommand) =>
+        command.owner === username,
+    );
+    if (commandIndex === -1) return;
+    const command = this.undoStack.splice(commandIndex, 1)[0];
+    command.undo(this.namespace);
+    this.redoStack.push(command);
   }
 
   redo(username: string) {
-    if (this.redoStack.length === 0) return
-    const commandIndex = this.redoStack.findLastIndex((command: (DrawCommand | MoveCommand)) => command.owner === username)
-    if (!commandIndex) return
-    const command = this.redoStack.splice(commandIndex, 1)[0]
-    command.redo(this.namespace)
-    let spliceIndex = 0
+    if (this.redoStack.length === 0) return;
+    const commandIndex = this.redoStack.findLastIndex(
+      (command: DrawCommand | MoveCommand | EraseCommand) =>
+        command.owner === username,
+    );
+    if (commandIndex === -1) return;
+    const command = this.redoStack.splice(commandIndex, 1)[0];
+    command.redo(this.namespace);
+    let spliceIndex = 0;
     // Find the index to insert the command into the undo stack
     for (let i = this.undoStack.length - 1; i >= 0; i--) {
       if (this.undoStack[i].commandId < command.commandId) {
-        spliceIndex = i + 1
-        break
+        spliceIndex = i + 1;
+        break;
       }
     }
-    this.undoStack.splice(spliceIndex, 0, command)
+    this.undoStack.splice(spliceIndex, 0, command);
   }
 }
