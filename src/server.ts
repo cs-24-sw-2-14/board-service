0262a665191e912fc813fbc9c6bb3de679e21732
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-08 07:02:27 +0200

    set statuscode before sending response object

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express, { Express, Request, Response } from "express";
-import { createServer } from "http"
-var cors = require('cors');
+import { createServer } from "http";
+var cors = require("cors");
 import { Server } from "socket.io";
-import { Boards } from "./boards"
+import { Boards } from "./boards";
 
 const app: Express = express();
 app.use(express.json());
@@ -13,43 +13,43 @@ const socketIoPort = 6123;
 const httpServer = createServer(app);
 const socketio = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173"
-  }
+    origin: "http://localhost:5173",
+  },
 });
 
-let boards: Boards = new Boards(socketio)
+let boards: Boards = new Boards(socketio);
 
 app.post("/v1/board/create", (_, res: Response) => {
   res.send({ boardId: boards.createBoard() });
-  return
+  return;
 });
 
 app.post("/v1/board/join", (req: Request, res: Response) => {
-  const body = req.body
-  const board = boards.findBoard(body.boardId)
+  const body = req.body;
+  const board = boards.findBoard(body.boardId);
   if (!board) {
-    res.send("Board does not exist").status(404);
-    return
+    res.status(404).send("Board does not exist");
+    return;
   }
-  const userExists = board.findUser(body.username)
+  const userExists = board.findUser(body.username);
   if (!userExists) {
-    board.createUser(body.username)
-    res.send("Board is valid and username was added to board").status(200);
-    return
+    board.createUser(body.username);
+    res.send("Board is valid and username was added to board");
+    return;
   }
-  res.send("Board and username is valid").status(200);
-  return
+  res.send("Board and username is valid");
+  return;
 });
 
 app.post("/v1/board/validate", (req: Request, res: Response) => {
-  const body = req.body
-  const board = boards.findBoard(body.boardId)
+  const body = req.body;
+  const board = boards.findBoard(body.boardId);
   if (!board) {
-    res.send("Board does not exist").status(404);
-    return
+    res.status(404).send("Board does not exist");
+    return;
   }
-  res.send("Board is valid").status(200);
-  return
+  res.status(200).send("Board is valid");
+  return;
 });
 
 socketio.listen(socketIoPort);
