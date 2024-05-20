42c522ca6a12d09b8b8e8f79f37dc9302bf927a8
Mads Ludvig Timm Fagerlund <35049585+MadsLudvig@users.noreply.github.com>
2024-05-20 11:24:27 +0200

    Convert POST to GET requests (#49)
    * Convert some post requests to get requests
    
    * remove TSDOC

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { createServer } from "node:http";
 var cors = require("cors");
 import { Server } from "socket.io";
 import { Boards } from "./boards";
+import { BoardId, Username } from "./types";
 const PORT = 5123;
 
 var corsOptions = {
@@ -20,18 +21,14 @@ const socketio = new Server(server, {
 });
 let boards: Boards = new Boards(socketio);
 
-/**
- * Creates a new board
- * @returns boardId - The unique id identifying the new board created
- */
 app.post("/v1/board/create", (_, res: Response) => {
   res.send({ boardId: boards.createBoard() });
   return;
 });
 
-app.post("/v1/board/validateBoardId", (req: Request, res: Response) => {
-  const body = req.body;
-  if (!boards.boards.has(body.boardId)) {
+app.get("/v1/board/validateBoardId", (req: Request, res: Response) => {
+  const boardId = req.query.boardId as BoardId;
+  if (!boards.boards.has(boardId)) {
     res.status(404).send("Board does not exist");
     return;
   }
@@ -39,39 +36,48 @@ app.post("/v1/board/validateBoardId", (req: Request, res: Response) => {
   return;
 });
 
-app.post("/v1/board/validateUsername", (req: Request, res: Response) => {
-  const body = req.body;
-  if (body.boardId === undefined || body.username === undefined) {
+app.get("/v1/board/validateUsername", (req: Request, res: Response) => {
+  const boardId = req.query.boardId as BoardId;
+  const username = req.query.username as Username;
+  if (boardId === undefined || username === undefined) {
     res.status(400).send("Bad Request: Missing boardId or username");
     return;
   }
-  if (!boards.boards.has(body.boardId)) {
+  if (!boards.boards.has(boardId)) {
     res.status(400).send("Bad Request: Board does not exist");
     return;
   }
-  const board = boards.boards.get(body.boardId)!;
+  const board = boards.boards.get(boardId)!;
 
-  if (board.users.has(body.username)) {
+  if (board.users.has(username)) {
     res.status(409).send("Conflict: Username is Already taken");
     return;
   }
   res.status(200).send("Username is available");
 });
 
-app.post("/v1/board/validateColor", (req: Request, res: Response) => {
-  const body = req.body;
-  if (body.boardId === undefined || body.color === undefined) {
+app.get("/v1/board/validateColor", (req: Request, res: Response) => {
+  const boardId = req.query.boardId as BoardId;
+  const colorString = req.query.color as string;
+
+  if (boardId === undefined || colorString === undefined) {
     res.status(400).send("Bad Request: Missing boardId or color");
     return;
   }
-  if (!boards.boards.has(body.boardId)) {
+  const color = parseInt(colorString);
+  if (isNaN(color)) {
+    res.status(400).send("Bad Request: Color format invalid");
+    return;
+  }
+
+  if (!boards.boards.has(boardId)) {
     res.status(400).send("Bad Request: Board does not exist");
     return;
   }
-  const board = boards.boards.get(body.boardId)!;
+  const board = boards.boards.get(boardId)!;
 
-  for (const user of board.users) {
-    if (user[1].color === body.color) {
+  for (const [_, user] of board.users) {
+    if (user.color === color) {
       res.status(409).send("Conflict: Color is Already taken");
       return;
     }
