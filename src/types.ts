9125f986aa35be1a8ff61ad4aee1d9b097c8b566
Cecilie Lassen <144027529+Ceci217c@users.noreply.github.com>
2024-08-14 12:05:15 +0200

    Changed types and comments (#57)
    commandId: number changed to commandId: CommandId.
    owner: string changed to owner: Username.
    Made type TextString there is a string.
    content: string changed to content: TextString.
    Some comments and rearranging.

--- src/types.ts
+++ src/types.ts
@@ -26,6 +26,7 @@ export type Threshold = number;
 export type SvgString = string;
 export type BoardId = string;
 export type Username = string;
+export type TextString = string; 
 
 export type CoordinateSet = {
   x: number;
@@ -35,10 +36,17 @@ export type CoordinateSet = {
 export type CanvasCoordinateSet = CoordinateSet;
 
 export interface Command {
+  // The interface Command consists of commandId, owner, done, execute, undo and redo.
   commandId: CommandId;
+  // commandId is of the type CommandId (number). 
   owner: Username;
+  // owner is of the type Username (string). 
   done: Boolean;
+  // done is of the type Boolean (true or false). 
   execute: (socket: Namespace | Socket) => void;
+  // Function execute takes in a socket of the type Namespace or Socket and returns void (noting). 
   undo: (socket: Namespace | Socket) => void;
+  // Function undo takes in a socket of the type Namespace or Socket and returns void (noting).
   redo: (socket: Namespace | Socket) => void;
+  // Function redo takes in a socket of the type Namespace or Socket and returns void (noting). 
 }
